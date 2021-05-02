47ecffce71277d79c9429ab275c1944a81e56688
Kishspawn <57330562+Kishspawn@users.noreply.github.com>
2021-05-02 02:38:53 -0700

    Dog Eats
    TO DO:
    -Fix animation
    -Fix pick up ball
    -Fix ground
    -UI
    -Maybe water

--- Assets/Scripts/AIMgr.cs
+++ Assets/Scripts/AIMgr.cs
@@ -16,16 +16,18 @@ public class AIMgr : MonoBehaviour
     public Transform player;
     public float theDistance = 5f;
     public GameObject ball;
-    private float timer = 15f;
-    private bool timerOn = false; 
+    private float timer = 5f;
+    private bool timerOn = false;
     Animator anim;
+    public GameObject bowl;
+    bool isFood = false;
 
-    public Transform dog; 
+    public Transform dog;
     void Awake()
     {
         inst = this;
         anim = GetComponent<Animator>();
-       
+
     }
     // Start is called before the first frame update
     void Start()
@@ -36,22 +38,80 @@ public class AIMgr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         if (SteamVR_Actions._default.Whistle.GetState(SteamVR_Input_Sources.Any))
-         {
+        if (SteamVR_Actions._default.Whistle.GetState(SteamVR_Input_Sources.Any))
+        {
             //Debug.Log("Whistle??");
             CancelInvoke("GetAttention");
+            CancelInvoke("GoToBowl");
             Follow();
-         }
+        }
     }
-    public void GetAttention()
+
+    //Fetch Functions
+    public void playerBall()
     {
-        //Debug.Log(Vector3.Distance(transform.position, ball.transform.position));
-        if (Vector3.Distance(transform.position, ball.transform.position) >= 2)
+        //Player picks up ball
+        InvokeRepeating("Follow", 0.0f, 0.01667f);
+    }
+    public void Fetch()
+    {
+        //Begins Fetch
+       // CancelInvoke("Follow");
+        InvokeRepeating("GetAttention", 0.0f, 0.01667f);
+    }
+
+    public void GetAttention()
+     {
+            //Dog Goes to ball
+            if (Vector3.Distance(transform.position, ball.transform.position) >= 2)
+            {
+                speed = 3f;
+                anim.SetBool("isMoving", true);
+                transform.position = Vector3.MoveTowards(transform.position, ball.transform.position, speed * Time.deltaTime);
+                var rotation = Quaternion.LookRotation(ball.transform.position - transform.position);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotateSpeed * Time.deltaTime);
+            }
+            else
+            {
+
+                speed = 0f;
+                anim.SetBool("isMoving", false);
+                PickUpBall();
+
+            }
+     }
+
+    public void PickUpBall()
+    {
+        //Dog picks up ball
+        CancelInvoke("GetAttention");
+        Debug.Log("PickUp");
+        ball.transform.SetParent(dog);
+        //ball.transform.localPosition = Vector3.zero;
+        InvokeRepeating("Follow", 0.0f, 0.01667f);
+
+    }
+    public void PutDownBall()
+    {
+        //Dog puts down ball
+        Debug.Log("PutDown");
+        ball.transform.SetParent(null);
+        CancelInvoke("Follow");
+    }
+
+    //Used for Fetch and Whistle
+    public void Follow()
+    {
+        //Dog Goes to person
+        Debug.Log("Follow");
+
+        if (Vector3.Distance(transform.position, player.position) >= theDistance)
         {
-            speed = 3f;
+
+            speed = 2f;
             anim.SetBool("isMoving", true);
-            transform.position = Vector3.MoveTowards(transform.position, ball.transform.position, speed * Time.deltaTime);
-            var rotation = Quaternion.LookRotation(ball.transform.position - transform.position);
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+            var rotation = Quaternion.LookRotation(player.transform.position - transform.position);
             transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotateSpeed * Time.deltaTime);
         }
         else
@@ -59,73 +119,84 @@ public class AIMgr : MonoBehaviour
 
             speed = 0f;
             anim.SetBool("isMoving", false);
-            PickUpBall();
+            PutDownBall();
 
         }
-    }
-    public void Fetch()
-    {
-        //Debug.Log("GetAttention");
-        InvokeRepeating("GetAttention", 0.0f, 0.01667f);
-    }
-    public void PickUpBall()
-    {
-        CancelInvoke("GetAttention");
-        Debug.Log("PickUp");
-        ball.transform.SetParent(dog);
-        //ball.transform.localPosition = Vector3.zero;
-        InvokeRepeating("Follow", 0.0f, 0.01667f);
-        
-    }
-    public void PutDownBall()
-    {
-        Debug.Log("PutDown");
-        ball.transform.SetParent(null);
-        CancelInvoke("Follow");
-    }
-    public void Follow()
-    {
-        Debug.Log("Follow");
 
-        if (Vector3.Distance(transform.position, player.position) >= theDistance)
-            {
-                
-                speed = 2f;
-                anim.SetBool("isMoving", true);
-                transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
-                var rotation = Quaternion.LookRotation(player.transform.position - transform.position);
-                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotateSpeed * Time.deltaTime);
-            }
-            else
-            {
-                
-                speed = 0f;
-                anim.SetBool("isMoving", false);
-                PutDownBall();
-
-        }
-              
     }
+
+    //Petting functions
+
     public void PetDog()
     {
-        anim.SetBool("isPet", true);
+        //Person pets dog
+        if(anim.GetBool("isMoving") == false && anim.GetBool("isEat") == false)
+        {
+            anim.SetBool("isPet", true);
+        }
+
+
     }
     public void StopPetDog()
     {
+        //Person stops petting dog
         anim.SetBool("isPet", false);
     }
-    //Timer
-    void BegToPlayTimer()
+
+
+    //Food bowl functions
+    public void EatBowl()
     {
+        //Begins the dog eating fod
+        InvokeRepeating("GoToBowl", 0.0f, 0.01667f);
+    }
+    public void GoToBowl()
+    {
+        //Dog goes to bowl
+        if (Vector3.Distance(transform.position, bowl.transform.position) >= 1)
+        {
+            speed = 2f;
+            anim.SetBool("isMoving", true);
+            transform.position = Vector3.MoveTowards(transform.position, bowl.transform.position, speed * Time.deltaTime);
+            var rotation = Quaternion.LookRotation(bowl.transform.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotateSpeed * Time.deltaTime);
+        }
+        else
+        {
+            speed = 0f;
+            anim.SetBool("isMoving", false);
+            //Dog eats food
+            anim.SetBool("isEat", true);
+            timerOn = true;
+            InvokeRepeating("myTimer", 0.0f, 0.01667f);
+        }
+    }
+
+    public void EndEat()
+    {
+        //Dog finishes food
+        CancelInvoke("myTimer");
+        anim.SetBool("isEat", false);
+        Bowl.inst.food.SetActive(false);
+        timer = 5f;
+    }
+
+    //Timer
+    void myTimer()
+    {
+        //Timer to give a little time for animation
+        Debug.Log("timer");
+        CancelInvoke("GoToBowl");
         if (timerOn == true)
         {
             timer -= Time.deltaTime;
             if (timer <= 0) //0 minutes terminates
             {
-                anim.SetBool("isBored", true);
-                
+
+                EndEat();
+               // timerOn = false;
             }
-            //anim.SetBool("isBored", false);
+            
         }
 
     }
